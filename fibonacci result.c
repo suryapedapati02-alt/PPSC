07beb47964e64fd4e1ca166f54e1dddf33d97749
suryapedapati02-alt <suryapedapati02@gmail.com>
2025-12-04 21:38:19 +0530

    Add files via upload

---
+++ fibonacci result.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+	if(n <=1)
+	return n;
+	else
+	return fibonacci(n-1) + fibonacci(n -2);
+
+}
+int main()
+{
+	int i,n;
+	printf("enter the number of terms in the fibonacci sequence:");
+	scanf("%d",&n);
+	printf("fibonacci sequence:\n");
+	for (i=0;i< n;i++)
+	printf("%d",fibonacci(i));
+	return 0;
+}
